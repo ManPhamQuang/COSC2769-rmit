dcc7f771ae07e3cb3a44fd73b7fc5836449e74a8
Vu Thang <dangvuthang97@gmail.com>
2021-07-13 03:28:25 +0700

    Add error handler

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
+const roomRoute = require("./route/roomRoute");
+const userRoute = require("./route/userRoute");
+const AppError = require("./util/appError");
+const GlobalErrorController = require("./controller/errorController");
+app.use(express.json());
 
 app.use("/helloworld", (req, res) => {
   res.status(200).json({
     message: "Test successfully",
   });
 });
+
+app.use("/api/v1/rooms", roomRoute);
+app.use("/api/v1/users", userRoute);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Given url ${req.originalUrl} does not exist`, 404));
+});
+
+app.use(GlobalErrorController);
 
 module.exports = app;
