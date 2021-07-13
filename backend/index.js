30e03bd35cc5e9e79077a8c11fa3d27a846c7410
Vu Thang <dangvuthang97@gmail.com>
2021-07-13 21:02:54 +0700

    Add cors, bcrypt, and jsonwebtoken package

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
 const roomRoute = require("./route/roomRoute");
 const userRoute = require("./route/userRoute");
 const AppError = require("./util/appError");
 const GlobalErrorController = require("./controller/errorController");
+const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/helloworld", (req, res) => {
   res.status(200).json({
     message: "Test successfully",
