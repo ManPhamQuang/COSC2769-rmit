05dd4292be5b57762b719147ef189383a1d16e88
Man Pham <s3804811@rmit.edu.vn>
2021-07-20 00:11:40 +0700

    added hover tooltip

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,39 +1,96 @@
 import React from "react";
 import { StarIcon } from "@heroicons/react/solid";
+import { createPopper } from "@popperjs/core";
+
+const color = "pink";
 
 export default function Card({ props }) {
-    const { id, title, teacher, rating, views, price, tag } = props;
+    const { id, title, teacher, rating, views, price, tag, description } =
+        props;
+
+    const [tooltipShow, setTooltipShow] = React.useState(false);
+    const cardRef = React.createRef();
+    const tooltipRef = React.createRef();
+    const openLeftTooltip = () => {
+        createPopper(cardRef.current, tooltipRef.current, {
+            placement: "right",
+        });
+        setTooltipShow(true);
+    };
+    const closeLeftTooltip = () => {
+        setTooltipShow(false);
+    };
 
     return (
-        <div className="flex-none w-64 group cursor-pointer inline-block">
-            <div className="h-36 w-full group-hover:bg-gray-50 group-hover:border-gray-50 rounded-md">
-                <img
-                    src="/Python-language.png"
-                    className="object-cover border-gray-300 border rounded-md h-full w-full group-hover:bg-red-400 mix-blend-multiply"
-                />
-            </div>
+        <>
+            <div
+                className="flex-none w-64 group cursor-pointer inline-block"
+                ref={cardRef}
+                onMouseEnter={openLeftTooltip}
+                onMouseLeave={closeLeftTooltip}
+            >
+                <div className="h-36 w-full group-hover:bg-gray-50 group-hover:border-gray-50 rounded-md">
+                    <img
+                        src="/Python-language.png"
+                        className="object-cover border-gray-300 border rounded-md h-full w-full group-hover:bg-red-400 mix-blend-multiply"
+                    />
+                </div>
 
-            <div className="leading-snug mt-2">
-                <h1 className="font-bold">{title}</h1>
-                <p className="text-sm font-normal text-gray-500 truncate">
-                    {teacher}
-                </p>
-                <p className="text-yellow-700 font-semibold inline">{rating}</p>
-                <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
-                <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
-                <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
-                <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
-                <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
-                <p className="inline text-sm font-normal text-gray-500">
-                    ({views})
-                </p>
-                <h1 className="font-bold">{price}</h1>
-                <div className="py-1 bg-yellow-200 w-16 rounded-sm">
-                    <p className="text-xs font-semibold mx-auto text-center">
-                        {tag}
+                <div className="leading-snug mt-2">
+                    <h1 className="font-bold">{title}</h1>
+                    <p className="text-sm font-normal text-gray-500 truncate">
+                        {teacher}
                     </p>
+                    <p className="text-yellow-700 font-semibold inline">
+                        {rating}
+                    </p>
+                    <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
+                    <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
+                    <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
+                    <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
+                    <StarIcon className="h-4 w-4 text-yellow-500 inline mb-1" />
+                    <p className="inline text-sm font-normal text-gray-500">
+                        ({views})
+                    </p>
+                    <h1 className="font-bold">{price}</h1>
+                    <div className="py-1 bg-yellow-200 w-16 rounded-sm">
+                        <p className="text-xs font-semibold mx-auto text-center">
+                            {tag}
+                        </p>
+                    </div>
                 </div>
             </div>
-        </div>
+            {/* Tooltip section - appears on card hover */}
+            <div
+                className={
+                    tooltipShow
+                        ? `bg-gray-50 border-0 ml-3 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-lg`
+                        : `hidden bg-${color}-600 border-0 ml-3 block z-50 font-normal leading-normal text-sm max-w-xs text-left no-underline break-words rounded-lg`
+                }
+                ref={tooltipRef}
+            >
+                <div>
+                    <div
+                        className={`bg-${color}-600 opacity-75 font-semibold p-3 mb-0 border-b border-solid border-blueGray-100 uppercase rounded-t-lg`}
+                    >
+                        {color} tooltip title
+                    </div>
+                    <h1 className="p-3 font-bold">{title}</h1>
+                    <div className="p-3">{description}</div>
+                </div>
+                {/* <div
+                    style={{
+                        width: 0,
+                        height: 0,
+                        borderTop: "8px solid transparent",
+                        borderRight: "17px solid #555",
+                        borderBottom: "8px solid transparent",
+                        position: "absolute",
+                        top: "80px",
+                        left: -15,
+                    }}
+                ></div> */}
+            </div>
+        </>
     );
 }
--- frontend/components/CardSlider.js
+++ frontend/components/CardSlider.js
@@ -10,6 +10,8 @@ const data = [
         views: "237,548",
         price: "$94,99",
         tag: "Bestseller",
+        description:
+            "Become a full-stack web developer with just one course. HTML, CSS, Javascript, Node, React, MongoDB and more!",
     },
     {
         id: 2,
@@ -20,6 +22,8 @@ const data = [
         views: "289,029",
         price: "$94,99",
         tag: "Bestseller",
+        description:
+            "Learn Python like a Professional Start from the basics and go all the way to creating your own applications and games",
     },
     {
         id: 3,
@@ -29,6 +33,8 @@ const data = [
         views: "237,548",
         price: "$94,99",
         tag: "Bestseller",
+        description:
+            "Dive in and learn React.js from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js and way more!",
     },
     {
         id: 4,
@@ -38,19 +44,12 @@ const data = [
         views: "630,406",
         price: "$94,99",
         tag: "Bestseller",
+        description:
+            "Dive in and learn React.js from scratch! Learn Reactjs, Hooks, Redux, React Routing, Animations, Next.js and way more!",
     },
 ];
 
 export default function CardSlider() {
-    // return (
-    //     <div>
-    //         <Slider {...settings}>
-    //             {data.map((course) => (
-    //                 <Card key={course.id} props={course} />
-    //             ))}
-    //         </Slider>
-    //     </div>
-    // );
     return (
         <div className="space-x-5 flex overflow-x-auto">
             {data.map((course) => (
