b82bad684e835846e52c166dbec7d7a56d3c8234
Nghia Tran <vinhnghiatran@gmail.com>
2021-07-19 15:29:01 +0700

    Create new Category

--- backend/controller/categoryController.js
+++ backend/controller/categoryController.js
@@ -11,3 +11,34 @@ exports.getAllCategory = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.createCategory = catchAsync(async (req, res, next) => {
+  // Get name in the body
+  const name = req.body.name;
+  if (!name) {
+    return res.status(400).json({
+      status: "error",
+      message: "Missing Name in body",
+    });
+  }
+
+  // Find if it exist
+  var category = await Category.Adventure.findOne({name});
+  if (category) {
+    return res.status(200).json({
+      status: "success",
+      data: {
+        category,
+      },
+    });
+  }
+
+  // Create new one
+  category = await Category.create({name})
+  res.status(201).json({
+    status: "success",
+    data: {
+      category,
+    },
+  });
+});
--- backend/route/categoryRoute.js
+++ backend/route/categoryRoute.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getAllCategory
+  getAllCategory,
+  createCategory
 } = require("../controller/categoryController");
 
 router.get("/", getAllCategory);
+router.post("/", createCategory);
 
 module.exports = router;
