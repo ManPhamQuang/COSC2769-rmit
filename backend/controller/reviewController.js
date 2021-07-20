cbf4a1565cee17014b1b2c3dbcb49a703c920fef
Vu Thang <dangvuthang97@gmail.com>
2021-07-20 16:00:17 +0700

    Create new doc when update a review, and run validator again

--- backend/controller/reviewController.js
+++ backend/controller/reviewController.js
@@ -2,11 +2,13 @@ const Review = require("../model/Review");
 const catchAsync = require("../util/catchAsync");
 const AppError = require("../util/appError");
 
-exports.createReview = catchAsync((req, res, next) => {
+exports.createReview = catchAsync(async (req, res, next) => {
+  // Test
+  const from = req.body.from;
   const review = await Review.create({
     ...req.body,
-    from: req.params.teacherId,
-    to: req.user.id,
+    from,
+    to: req.params.teacherId,
   });
 
   res.status(201).json({
@@ -17,23 +19,30 @@ exports.createReview = catchAsync((req, res, next) => {
   });
 });
 
-exports.getAllReviews = catchAsync((req, res, next) => {
-  const review = await Review.find();
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  const reviews = await Review.find({ to: req.params.teacherId });
 
   res.status(200).json({
     status: "success",
+    length: reviews.length,
     data: {
-      review,
+      reviews,
     },
   });
 });
 
-exports.updateReview = catchAsync((req, res, next) => {
-  const review = await Review.findByIdAndUpdate(req.params.id, {
-    ...req.body,
-    from: req.params.teacherId,
-    to: req.user.id,
-  });
+exports.updateReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findByIdAndUpdate(
+    req.params.reviewId,
+    {
+      review: req.body.review,
+      rating: req.body.rating,
+    },
+    {
+      runValidators: true,
+      new: true,
+    }
+  );
   if (!review)
     return next(new AppError("No review was found with a given ID", 404));
   res.status(200).json({
@@ -44,8 +53,8 @@ exports.updateReview = catchAsync((req, res, next) => {
   });
 });
 
-exports.deleteReview = catchAsync((req, res, next) => {
-  const review = await Review.findByIdAndDelete(req.params.id);
+exports.deleteReview = catchAsync(async (req, res, next) => {
+  const review = await Review.findByIdAndDelete(req.params.reviewId);
   if (!review)
     return next(new AppError("No review was found with a given ID", 404));
   res.status(204).end();
