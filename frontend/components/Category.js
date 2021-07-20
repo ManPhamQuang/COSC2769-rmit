5fb391a6a09fb012fa1a43a57832fbbb85d59332
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2021-07-20 09:55:10 +0700

    tablet view

--- frontend/components/Category.js
+++ frontend/components/Category.js
@@ -11,22 +11,22 @@ export default function Category() {
     });
 
     return (
-        <div className="flex flex-wrap sm:overflow-x-auto">
+        <div className="flex flex-wrap justify-around lg:justify-start">
             {categories.map((category) => (
                 <React.Fragment key={category._id}>
-                    <div className="hidden md:inline-block flex-none w-80 group cursor-pointer mr-5 mb-4">
-                        <div className="h-60 w-full">
+                    <div className="hidden sm:inline-block flex-none w-52 group cursor-pointer mb-5 lg:mr-4 inline-block w-80 flex-none group cursor-pointer mb-5 ">
+                        <div className="h-40 w-full lg:h-60">
                             <img
                                 src="/Python-language.png"
-                                className="object-cover border rounded-md h-full w-full mix-blend-multiply transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 ..."
+                                className="object-cover border rounded-md h-full w-full mix-blend-multiply lg:transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
                             />
                         </div>
                         <div className="mt-2">
                             <h1 className="font-bold">{category.name}</h1>
                         </div>
                     </div>
-                    <div className="flex-initial mt-2 mb-2 md:hidden">
-                        <h1 className="rounded-full border py-1 px-3 mr-2 font-bold border-black hover:bg-gray-200 flex items-center justify-center">
+                    <div className="flex-initial mt-2 mb-2 sm:hidden">
+                        <h1 className="rounded-full border py-1 px-2 mr-1 font-bold border-black hover:bg-gray-200 flex items-center justify-center text-center">
                             {category.name}
                         </h1>
                     </div>
