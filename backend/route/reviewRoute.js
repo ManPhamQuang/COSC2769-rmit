d248a4952df86e17c5fc9f46f94cc84b6e381546
Vu Thang <dangvuthang97@gmail.com>
2021-07-20 17:04:32 +0700

    Constraint:  Only login users can give review

--- backend/route/reviewRoute.js
+++ backend/route/reviewRoute.js
@@ -5,8 +5,10 @@ const {
   updateReview,
   deleteReview,
 } = require("../controller/reviewController");
+const { isAuthenticated } = require("../controller/authController");
 const router = express.Router({ mergeParams: true });
-
+// Only login user can give review
+router.use(isAuthenticated);
 router.get("/", getAllReviews);
 router.post("/", createReview);
 router.patch("/:reviewId", updateReview);
