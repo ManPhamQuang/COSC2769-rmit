c8aa5730c82d95f2d92eaec6f115edbb7b72e009
Vu Thang <dangvuthang97@gmail.com>
2021-07-20 15:57:56 +0700

    Fixed not find viewRoute

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { createUser, login } = require("../controller/authController");
-
-router.use("/:teacherId/reviews", reviewRouter);
+const reviewRoute = require("./reviewRoute");
+// Create merge params for this resource => Route: /api/v1/users/:teacherId/reviews
+router.use("/:teacherId/reviews", reviewRoute);
 router.post("/signup", createUser);
 router.post("/login", login);
 module.exports = router;
