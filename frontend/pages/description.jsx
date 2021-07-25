3c5ef32f3f256ade971243d6da1d3cbabc77e9f7
NguyenDangLamPhuong <s3754105@rmit.edu.vn>
2021-07-25 13:09:32 +0700

    add room detaail in roomDescription Page

--- frontend/pages/description.jsx
+++ frontend/pages/description.jsx
@@ -1,5 +1,6 @@
 import React,{ useState, useRef, useReducer } from "react";
 import TeacherSelfIntroduction from "../components/TeacherSelfIntroduction";
+
 import {
     StarIcon,
     ExclamationCircleIcon,
@@ -29,12 +30,8 @@ const courseInfor = [
         ],
     },
 ];
-const teacherInfor = [
-    {
-        id: 1,
-        name: "Kyle Pew",
-    },
-];
+
+
 const relevantCourses = [
     {
         id: 1,
@@ -79,13 +76,26 @@ const relevantCourses = [
 const teacherSelfIntroduction = [
     {
         id: 1,
+        avatar:"https://img-c.udemycdn.com/user/200_H/15722848_5948_3.jpg",
         name: "Angela",
         job:"Developer and Lead Instructor",
         rating: 5,
+        roomAmount:80,
+        reviewerAmount:"314,629",
         seftIntroduction:
             "I'm Angela, I'm a developer with a passion for teaching. I'm the lead instructor at the London App Brewery, London's leading Programming Bootcamp. I've helped hundreds of thousands of students learn to code and change their lives by becoming a developer. I've been invited by companies such as Twitter, Facebook and Google to teach their employees.",
     },
 ];
+const listFeatures=courseInfor.features;
+// const feature={courseInfor.map((listFeatures, index) => {
+//       return (
+//      <ol>
+//         {items.map((subItems, sIndex) => {
+//               return <li> {subItems} </li>;
+//            })}
+//          </ol>
+//      );
+// })}
 
 export default function description() {
     return (
@@ -139,8 +149,35 @@ export default function description() {
                 </div>
             </div>
             
-            <div className="container mx-auto p-14">
-            <div class="uppercase tracking-wide text-lg dark:text-gray-200 font-semibold">Instructors</div>
+            <div className="container mx-auto p-14">          
+                <div className="relative flex flex-col ">
+                    <div class="uppercase tracking-wide text-lg dark:text-gray-200 font-semibold">What you'll learn</div>
+
+                    <div class="bg-white dark:bg-gray-800 w- rounded-lg p-4 mb-6 shadow sm:inline-block">
+                            <div class="md:flex items-start md:text-left">
+                                <div class="md:w-1/2 px-3 mb-6 md:mb-5 inline" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>
+                                    <p class="text-red text-s inline ml-1">Concepts of Object Oriented Programming (OOP): The type system, variables, functions and methods, inheritance, structures, classes and protocols.</p>
+                                </div>
+                                <div class="md:w-1/2 px-3 mb-6 md:mb-5 inline" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>
+                                    <p class="text-red text-s inline ml-1">Concepts of Object Oriented Programming (OOP): The type system, variables, functions and methods, inheritance, structures, classes and protocols.</p>
+                                </div>
+                            </div>
+                            <div class="md:flex items-start md:text-left">
+                                <div class="md:w-1/2 px-3 mb-6 md:mb-5 inline" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>
+                                    <p class="text-red text-s inline ml-1">Concepts of Object Oriented Programming (OOP): The type system, variables, functions and methods, inheritance, structures, classes and protocols.</p>
+                                </div>
+                                <div class="md:w-1/2 px-3 mb-6 md:mb-5 inline" >
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" /></svg>
+                                    <p class="text-red text-s inline ml-1">Concepts of Object Oriented Programming (OOP): The type system, variables, functions and methods, inheritance, structures, classes and protocols.</p>
+                                </div>
+                            </div>
+                    </div>       
+                </div>
+                <div class="uppercase tracking-wide text-lg dark:text-gray-200 font-semibold">Instructors</div>
+
                 <div className="relative flex flex-col ">
                     <TeacherSelfIntroduction
                         key={teacherSelfIntroduction[0].id}
