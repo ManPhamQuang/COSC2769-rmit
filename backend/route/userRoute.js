ea064204eb673f0a6518acea71f21c6a905e6bd3
Vu Thang <dangvuthang97@gmail.com>
2021-07-28 15:28:23 +0700

    Add route to get current user's info

--- backend/route/userRoute.js
+++ backend/route/userRoute.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, login } = require("../controller/authController");
+const {
+  createUser,
+  login,
+  isAuthenticated,
+} = require("../controller/authController");
+const { getMe } = require("../controller/userController");
 const reviewRoute = require("./reviewRoute");
 // Create merge params for this resource => Route: /api/v1/users/:teacherId/reviews
 router.use("/:teacherId/reviews", reviewRoute);
 router.post("/signup", createUser);
 router.post("/login", login);
+router.get("/getMe", isAuthenticated, getMe);
 module.exports = router;
