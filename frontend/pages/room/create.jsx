d06442375ee52aaadce8ab9d5ac5485d8e8e0844
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-07-29 11:33:26 +0700

    Fix selected category value

--- frontend/pages/room/create.jsx
+++ frontend/pages/room/create.jsx
@@ -59,7 +59,7 @@ const data = {
 const Create = () => {
   // State to display dropdown options for categories
   const [categories, setCategories] = useState(INIT_CATEGORY);
-  const [selectedCategory, setSelectedCategory] = useState(categories[0].name);
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   // State to prepare data for server request 
   const [category, setCategory] = useState("");
@@ -128,6 +128,11 @@ const Create = () => {
     e.preventDefault();
   }
 
+  const categoryDropdownOnChange = (category) => {
+    console.log(category)
+    setSelectedCategory(category);
+  };
+
   return (
     <div className="container mt-20 mx-auto px-4 h-full">
       {room.error && (
@@ -182,8 +187,8 @@ const Create = () => {
                         <Listbox
                           as="div"
                           className="space-y-1"
-                          value={selectedCategory}
-                          onChange={setSelectedCategory}
+                          value={selectedCategory.name}
+                          onChange={categoryDropdownOnChange}
                         >
                           {({ open }) => (
                             <>
@@ -194,7 +199,7 @@ const Create = () => {
                                 <span className="inline-block w-full rounded-md shadow-sm">
                                   <Listbox.Button className="cursor-default relative w-full rounded-md border border-gray-300 bg-white pl-3 pr-10 py-2 text-left focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
                                     <span className="block truncate">
-                                      {selectedCategory}
+                                      {selectedCategory.name}
                                     </span>
                                     <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                       <svg
@@ -228,7 +233,7 @@ const Create = () => {
                                     {categories.map((category, index) => (
                                       <Listbox.Option
                                         key={index}
-                                        value={category.name}
+                                        value={category}
                                       >
                                         {({ selected, active }) => (
                                           <div
@@ -247,28 +252,6 @@ const Create = () => {
                                             >
                                               {category.name}
                                             </span>
-                                            {selected && (
-                                              <span
-                                                className={`${
-                                                  active
-                                                    ? "text-white"
-                                                    : "text-indigo-600"
-                                                } absolute inset-y-0 left-0 flex items-center pl-1.5`}
-                                              >
-                                                <svg
-                                                  className="h-5 w-5"
-                                                  xmlns="http://www.w3.org/2000/svg"
-                                                  viewBox="0 0 20 20"
-                                                  fill="currentColor"
-                                                >
-                                                  <path
-                                                    fillRule="evenodd"
-                                                    d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                                                    clipRule="evenodd"
-                                                  />
-                                                </svg>
-                                              </span>
-                                            )}
                                           </div>
                                         )}
                                       </Listbox.Option>
