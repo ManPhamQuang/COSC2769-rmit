47df13a36fbbd1188b93100eb3134c6411cbc57e
Man Pham <s3804811@rmit.edu.vn>
2021-07-31 15:21:15 +0700

    dashboard rooms table fetch data from api

--- frontend/components/RoomTable.js
+++ frontend/components/RoomTable.js
@@ -1,40 +1,51 @@
 import React from "react";
 import useBreakpoint from "../hooks/useBreakpoint";
+import useSWR from "swr";
+import axios from "axios";
+import DateFormatter from "../utils/DateFormat";
 
-/* This example requires Tailwind CSS v2.0+ */
-const rooms = [
-    {
-        _id: "53613131",
-        title: "Development Bootcamp",
-        beginAt: "20/07/2019",
-        status: "over",
-        category: "business",
-    },
-    {
-        _id: "1321321321",
-        title: "Agile Crash Course: Agile Project Management; Agile Delivery",
-        beginAt: "12/04/2020",
-        status: "pending",
-        category: "music",
-    },
-    {
-        _id: "2090909",
-        title: "JavaScript Projects Bootcamp:HTML, CSS, VanillaJS, Bootstrap",
-        beginAt: "18/06/2020",
-        status: "active",
-        category: "Fullstack Development",
-    },
-    {
-        _id: "1203921",
-        title: "Development Bootcamp",
-        beginAt: "20/01/2021",
-        status: "pending",
-        category: "Machine Learning",
-    },
-    // Mock data
-];
+const fetcher = (url, token) =>
+    axios
+        .get(url, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => res.data.data.rooms);
 
-export default function RoomTable() {
+export default function RoomTable({ user }) {
+    const token = localStorage.getItem("accessToken");
+    const { data: rooms, error } = useSWR(
+        [`http://localhost:5000/api/v1/rooms?createdBy=${user._id}`, token],
+        fetcher
+    );
+    const SkeletonRows = () => (
+        <tbody className="animate-pulse bg-white divide-y divide-gray-200">
+            <tr>
+                <td className="h-[41px] bg-gray-200 mb-6"></td>
+                <td className="h-[41px] bg-gray-300 mb-6"></td>
+                <td className="h-[41px] bg-red-200 mb-6"></td>
+                <td className="h-[41px] bg-gray-300 mb-6"></td>
+                <td className="h-[41px] bg-gray-200 mb-6"></td>
+            </tr>
+            <tr>
+                <td className="h-[41px] bg-gray-300 mb-6"></td>
+                <td className="h-[41px] bg-gray-200 mb-6"></td>
+                <td className="h-[41px] bg-green-100 mb-6"></td>
+                <td className="h-[41px] bg-gray-200 mb-6"></td>
+                <td className="h-[41px] bg-gray-300 mb-6"></td>
+            </tr>
+            <tr>
+                <td className="h-[41px] bg-gray-200 mb-6"></td>
+                <td className="h-[41px] bg-gray-300 mb-6"></td>
+                <td className="h-[41px] bg-yellow-100 mb-6"></td>
+                <td className="h-[41px] bg-gray-300 mb-6"></td>
+                <td className="h-[41px] bg-gray-200 mb-6"></td>
+            </tr>
+        </tbody>
+    );
+    if (error) {
+        return <h1>ERROR</h1>;
+    }
+
     return (
         <div className="p-5 lg:p-10">
             <div className="">
@@ -87,60 +98,70 @@ export default function RoomTable() {
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody className="bg-white divide-y divide-gray-200">
-                                    {rooms.map((room) => {
-                                        let color = "";
-                                        if (room.status == "active") {
-                                            color =
-                                                "bg-green-100 text-green-800";
-                                        } else if (room.status == "pending") {
-                                            color =
-                                                "bg-yellow-100 text-yellow-700";
-                                        } else {
-                                            color = "bg-red-200 text-red-700";
-                                        }
-                                        return (
-                                            <tr
-                                                key={room._id}
-                                                className="group cursor-pointer hover:bg-gray-200 cursor"
-                                            >
-                                                <td className="px-3 py-2 whitespace-nowrap">
-                                                    <div className="flex items-center">
-                                                        <div
-                                                            className={`text-sm text-gray-500 max-w-[160px] xl:max-w-none truncate`}
-                                                        >
-                                                            {room.title}
+
+                                {rooms ? (
+                                    <tbody className="bg-white divide-y divide-gray-200">
+                                        {rooms.map((room) => {
+                                            let color = "";
+                                            if (room.status == "active") {
+                                                color =
+                                                    "bg-green-100 text-green-800";
+                                            } else if (
+                                                room.status == "pending"
+                                            ) {
+                                                color =
+                                                    "bg-yellow-100 text-yellow-700";
+                                            } else {
+                                                color =
+                                                    "bg-red-200 text-red-700";
+                                            }
+                                            return (
+                                                <tr
+                                                    key={room._id}
+                                                    className="group cursor-pointer hover:bg-gray-200 cursor"
+                                                >
+                                                    <td className="px-3 py-2 whitespace-nowrap">
+                                                        <div className="flex items-center">
+                                                            <div
+                                                                className={`text-sm text-gray-500 max-w-[160px] xl:max-w-none truncate`}
+                                                            >
+                                                                {room.title}
+                                                            </div>
                                                         </div>
-                                                    </div>
-                                                </td>
-                                                <td className="px-3 py-2 whitespace-nowrap">
-                                                    <div className="text-sm text-gray-900">
-                                                        {room.beginAt}
-                                                    </div>
-                                                    {/* <div className="text-sm text-gray-500">{room.status}</div> */}
-                                                </td>
-                                                <td className="px-3 py-2 whitespace-nowrap">
-                                                    <span
-                                                        className={`px-2 inline-flex text-xs leading-5 font-semibold ${color} rounded-full`}
-                                                    >
-                                                        {room.status}
-                                                    </span>
-                                                </td>
-                                                <td className="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
-                                                    {room.category}
-                                                </td>
-                                                <td className="px-3 py-2 whitespace-nowrap text-right text-sm font-medium">
-                                                    <a
-                                                        href="#"
-                                                        className="text-indigo-600 hover:text-indigo-900"
-                                                    >
-                                                        Edit
-                                                    </a>
-                                                </td>
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
+                                                    </td>
+                                                    <td className="px-3 py-2 whitespace-nowrap">
+                                                        <div className="text-sm text-gray-900">
+                                                            {DateFormatter(
+                                                                room.startedAt
+                                                            )}
+                                                        </div>
+                                                        {/* <div className="text-sm text-gray-500">{room.status}</div> */}
+                                                    </td>
+                                                    <td className="px-3 py-2 whitespace-nowrap">
+                                                        <span
+                                                            className={`px-2 inline-flex text-xs leading-5 font-semibold ${color} rounded-full`}
+                                                        >
+                                                            {room.status}
+                                                        </span>
+                                                    </td>
+                                                    <td className="px-3 py-2 whitespace-nowrap text-sm text-gray-500">
+                                                        {room.category?.name}
+                                                    </td>
+                                                    <td className="px-3 py-2 whitespace-nowrap text-right text-sm font-medium">
+                                                        <a
+                                                            href="#"
+                                                            className="text-indigo-600 hover:text-indigo-900"
+                                                        >
+                                                            Edit
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                ) : (
+                                    <SkeletonRows />
+                                )}
                             </table>
                         </div>
                     </div>
--- frontend/pages/dashboard/index.jsx
+++ frontend/pages/dashboard/index.jsx
@@ -1,8 +1,23 @@
 import RoomTable from "../../components/RoomTable";
 import DashboardLayout from "../../components/DashboardLayout";
 import Head from "next/head";
+import axios from "axios";
+import useSWR from "swr";
+
+const fetcher = (url, token) =>
+    axios
+        .get(url, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => res.data.data);
 
 export default function ExpertDashboard() {
+    const token = localStorage.getItem("accessToken");
+    const { data: user, error } = useSWR(
+        ["http://localhost:5000/api/v1/users/getMe", token],
+        fetcher
+    );
+
     return (
         <>
             <Head>
@@ -12,9 +27,7 @@ export default function ExpertDashboard() {
                     content="initial-scale=1.0, width=device-width"
                 />
             </Head>
-            <div>
-                <RoomTable />
-            </div>
+            <div>{user ? <RoomTable user={user} /> : null}</div>
         </>
     );
 }
