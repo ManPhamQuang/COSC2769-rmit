031f0c47203fe32760c4a1a019b6200c5eabfc38
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-08-03 16:14:48 +0700

    Video chatroom navbar

--- frontend/components/VideoRoom.js
+++ frontend/components/VideoRoom.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Participant from "./Participant";
+import LocalParticipant from "./LocalParticipant";
+import VideoRoomNavBar from "./VideoRoomNavBar";
 
 const Room = ({ roomName, room, handleLogout }) => {
   const [participants, setParticipants] = useState([]);
@@ -24,15 +25,13 @@ const Room = ({ roomName, room, handleLogout }) => {
     };
   }, [room]);
 
-  const remoteParticipants = participants.map((participant) => (
-    <Participant key={participant.sid} participant={participant} />
-  ));
+  // const remoteParticipants = participants.map((participant) => (
+  //   <Participant key={participant.sid} participant={participant} />
+  // ));
 
   return (
     <div className="room">
-      <h2 className="text-2xl my-5">Room: {roomName}</h2>
-      <button onClick={handleLogout} className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Log out</button>
-      <div className="local-participant">
+      <div className="local-participant h-full">
         {room ? (
           <Participant
             key={room.localParticipant.sid}
@@ -42,8 +41,8 @@ const Room = ({ roomName, room, handleLogout }) => {
           ""
         )}
       </div>
-      <h3 className="text-2xl my-5">Remote Participants</h3>
-      <div className="remote-participants">{remoteParticipants}</div>
+      {/* <div className="remote-participants">{remoteParticipants}</div> */}
+      <VideoRoomNavBar roomName={roomName} handleLogout={handleLogout}/>
     </div>
   );
 };
