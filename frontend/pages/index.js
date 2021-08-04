a563f223ffa050466bce0bb61d96e27fcb1975e5
Man Pham <s3804811@rmit.edu.vn>
2021-08-04 13:56:53 +0700

    added upcoming, active and budget room lists

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import RoomCardsSlider from "../components/RoomCardsSlider";
+import Category from "../components/Category";
 
 export default function Home() {
     return (
@@ -12,15 +13,46 @@ export default function Home() {
                 />
             </Head>
             <div className="container mx-auto p-5 lg:p-14">
-                <h1 className="font-bold text-4xl mb-4">What to learn next</h1>
-                <h1 className="font-bold text-2xl mb-4">Upcoming rooms</h1>
-                <RoomCardsSlider
-                    params={{
-                        limit: 10,
-                        sort: "-startedAt",
-                        status: "pending",
-                    }}
-                />
+                <h1 className="font-bold text-4xl">What to learn next</h1>
+                <div className="mt-10">
+                    <h1 className="font-bold text-2xl m-4 mt-0">
+                        Upcoming rooms
+                    </h1>
+                    <RoomCardsSlider
+                        params={{
+                            limit: 10,
+                            sort: "-startedAt",
+                            status: "pending",
+                        }}
+                    />
+                </div>
+
+                <div className="mt-10">
+                    <h1 className="font-bold text-2xl m-4 mt-0">
+                        Active rooms
+                    </h1>
+                    <RoomCardsSlider
+                        params={{
+                            limit: 10,
+                            sort: "-startedAt",
+                            status: "active",
+                        }}
+                    />
+                </div>
+
+                <div className="mt-10">
+                    <h1 className="font-bold text-2xl m-4 mt-0">
+                        Budget rooms
+                    </h1>
+                    <RoomCardsSlider
+                        params={{
+                            limit: 10,
+                            sort: "-price",
+                        }}
+                    />
+                </div>
+
+                <Category />
             </div>
         </>
     );
