8d20ca689b722e7ef0bbd1c0910de3856966edab
Man Pham <s3804811@rmit.edu.vn>
2021-08-04 13:20:25 +0700

    added rooms slider component

--- frontend/components/CardSlider.js
+++ frontend/components/CardSlider.js
@@ -3,13 +3,16 @@ import Card from "./Card";
 import axios from "axios";
 import useSWR from "swr";
 import SkeletonCard from "./SkeletonCard";
+import Carousel from "react-grid-carousel";
+
 const fetcher = (url) => axios.get(url).then((res) => res.data);
 
 export default function CardSlider() {
     const { data, error } = useSWR(
-        "http://localhost:5000/api/v1/rooms",
+        "http://localhost:5000/api/v1/rooms?limit=10",
         fetcher
     );
+
     if (!data)
         // Renders a row of skeleton card to indicate loading
         return (
@@ -18,19 +21,48 @@ export default function CardSlider() {
                 <SkeletonCard />
                 <SkeletonCard />
                 <SkeletonCard />
-                <SkeletonCard />
-                <SkeletonCard />
-                <SkeletonCard />
-                <SkeletonCard />
             </div>
         );
     if (error) return <h1>error!</h1>;
-
     return (
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <Carousel
+            cols={5}
+            responsiveLayout={[
+                {
+                    breakpoint: 1600,
+                    cols: 5,
+                    rows: 1,
+                    gap: 10,
+                    loop: true,
+                },
+                {
+                    breakpoint: 1280,
+                    cols: 4,
+                    rows: 1,
+                    gap: 10,
+                    loop: true,
+                },
+                {
+                    breakpoint: 1100,
+                    cols: 3,
+                    rows: 1,
+                    gap: 10,
+                    loop: true,
+                },
+                {
+                    breakpoint: 768,
+                    cols: 2,
+                    rows: 1,
+                    gap: 10,
+                    loop: true,
+                },
+            ]}
+        >
             {data.data.rooms.map((room) => (
-                <Card key={room._id} props={room} />
+                <Carousel.Item key={room._id}>
+                    <Card key={room._id} props={room} />
+                </Carousel.Item>
             ))}
-        </div>
+        </Carousel>
     );
 }
