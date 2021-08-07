cc2080f74be78b8725ac9652e9b89dfc15c26234
Hang Nguyen <hanu.nguyenhang@gmail.com>
2021-08-07 14:41:05 +0700

    Add mock layout for room detail

--- frontend/pages/room/[_id].jsx
+++ frontend/pages/room/[_id].jsx
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
 import axios from "axios";
 import useSWR from "swr";
+import Card from "../../components/Card";
 
 const fetcher = (url) => axios.get(url).then((res) => res.data.data);
 
@@ -18,9 +18,18 @@ export default function RoomDetail() {
       <h1>Room Detail page</h1>
       <h2>{_id}</h2>
       {data && (
-        <div>
-          <h2>{data.room.title}</h2>
-          <h2>{data.room.description}</h2>
+        <div className="container mt-20 mx-auto px-4 h-full">
+          <div className="flex content-center items-center justify-center h-full ">
+            <div className="w-full lg:w-4/12 px-4">
+              <Card props={data.room} />
+              <button
+                className="bg-indigo-600 mt-4 text-white active:bg-indigo-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full "
+                type="button"
+              >
+                Join Room
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
