48d02c1e5dc74103d371cca6fb5ae41f3e8c7402
Vu Thang <dangvuthang97@gmail.com>
2021-08-11 09:04:27 +0700

    Style Chat for medium and large screen size

--- frontend/components/Chat.js
+++ frontend/components/Chat.js
@@ -4,11 +4,12 @@ import { useEffect, useState } from "react";
 const ChatAPI = require("twilio-chat");
 import { getTwilioToken } from "../utils/API";
 
-function Chat({ username, roomName, token }) {
+function Chat({ username, roomName, token, isHidden, closeChat }) {
     const [loading, setLoading] = useState(false);
     const [messages, setMessages] = useState([]);
     const [channel, setChannel] = useState(null);
     const [text, setText] = useState("");
+    const bottomChat = useRef(null);
 
     useEffect(async () => {
         setLoading(true);
@@ -29,6 +30,7 @@ function Chat({ username, roomName, token }) {
             // getting list of all messages since this is an existing channel
             const newMessages = await channel.getMessages();
             setMessages(newMessages.items || []);
+            bottomChat.current.scrollIntoView({ behavior: "smooth" });
         });
 
         try {
@@ -48,6 +50,7 @@ function Chat({ username, roomName, token }) {
                 );
             }
         }
+        // useEffect should return a clean up function if components ever unmount
     }, []);
 
     const updateText = (e) => setText(e);
@@ -67,41 +70,95 @@ function Chat({ username, roomName, token }) {
 
     const handleMessageAdded = (message) => {
         setMessages((messages) => [...messages, message]);
+        bottomChat.current.scrollIntoView({ behavior: "smooth" });
     };
 
     const sendMessage = () => {
         if (text) {
-            setLoading(true);
             channel.sendMessage(String(text).trim());
             setText("");
-            setLoading(false);
+            bottomChat.current.scrollIntoView({ behavior: "smooth" });
         }
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        sendMessage();
+    };
+
     return (
-        <div className="bg-gray-700 min-h-screen">
-            <p className="text-2xl">Chat Message</p>
-            <div className="w-2/3">
-                <div>
-                    {messages &&
-                        messages.map((message, index) => (
-                            <ChatItem
-                                key={index}
-                                message={message}
-                                username={username}
-                            />
-                        ))}
+        <div
+            className={`bg-white min-h-screen w-96 border border-[#E4E7E9] ${
+                isHidden ? "hidden" : "block"
+            }`}
+        >
+            <div className="bg-[#F4F4F6] px-3 py-5 border-b border-[#E4E7E9] shadow-sm">
+                <div className="flex justify-between">
+                    <p className="font-bold">Chat</p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 fill-current text-gray-500 cursor-pointer hover:text-gray-700 transition"
+                        onClick={closeChat}
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M6 18L18 6M6 6l12 12"
+                        />
+                    </svg>
                 </div>
-                <div className="flex space-x-4 my-4">
+            </div>
+            <div
+                className={`px-2 py-4 space-y-5 h-[73vh] border-b border-[#E4E7E9] shadow-sm ${
+                    loading ? "overflow-hidden" : "overflow-auto"
+                }`}
+            >
+                {loading && (
+                    <div className="flex items-center justify-center h-full">
+                        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4 text-center"></div>
+                    </div>
+                )}
+                {messages.map((message, index) => (
+                    <ChatItem
+                        key={index}
+                        message={message}
+                        username={username}
+                    />
+                ))}
+                <div ref={bottomChat}></div>
+            </div>
+            <div className="space-x-4 ">
+                <form className="px-3" onSubmit={handleSubmit}>
                     <input
                         type="text"
                         placeholder="Type Message"
                         onChange={(e) => updateText(e.target.value)}
                         value={text}
-                        className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        className="mt-5 focus:ring-blue-500 focus:border-blue-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     />
-                    <button onClick={sendMessage}>Send</button>
-                </div>
+                    <div className="flex mt-3">
+                        <button className="ml-auto focus:outline-none focus:rotate-90 rotate-45 hover:rotate-90 duration-300 ">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-6 w-6"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={1.5}
+                                    d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     );
