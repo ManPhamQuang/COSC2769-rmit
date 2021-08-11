a8db157152a753aaa4663c096857bf0c3ffe0c4b
Vu Thang <dangvuthang97@gmail.com>
2021-08-11 20:45:50 +0700

    Conditionally called scrollIntoView and disable input field when chat is loading

--- frontend/components/ChatItem.js
+++ frontend/components/ChatItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import dayjs from "dayjs";
 function ChatItem({ message, username }) {
     const isOwnMessage = message.author === username;
-    console.log(message);
     return (
         <div className="space-y-2">
             <div className="flex text-xs text-gray-500 justify-between">
@@ -15,10 +14,11 @@ function ChatItem({ message, username }) {
             </div>
             <div className="flex">
                 <p
-                    className={`rounded-xl p-3 text-sm 
-                      ${isOwnMessage ? "bg-[#E1E3EA]" : "bg-[#CCE4FF]"}
-                      ${isOwnMessage && "ml-auto"} 
+                    className={`rounded-xl p-3 text-sm break-words whitespace-pre-wrap break-all
+                      ${isOwnMessage ? "bg-[#E1E3EA]" : "bg-[#CCE4FF]"} 
+                      ${isOwnMessage && "ml-auto"}
                     `}
+                    style={{ hyphens: "auto" }}
                 >
                     {message.body}
                 </p>
