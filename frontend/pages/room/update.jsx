08fe42c1a11632e180af233ba59acf2b967b3204
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2021-08-16 00:03:32 +0700

    page init

---
+++ frontend/pages/room/update.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "../../components/navbar/NavBar";
+
+export default function update() {
+    return (
+        <div>
+            <NavBar />
+            <div className="container mx-auto p-4 h-full"></div>
+        </div>
+    );
+}
