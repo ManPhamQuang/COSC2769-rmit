0569f31154cc2d74a7e9d063a27e6198172d8e41
Vu Thang <dangvuthang97@gmail.com>
2021-08-24 17:44:53 +0700

    Refactor and set auto logout when user login, or signup

--- frontend/context/authContext/apiCalls.jsx
+++ frontend/context/authContext/apiCalls.jsx
@@ -6,8 +6,29 @@ import {
     getMeStart,
     getMeSuccess,
     getMeFailure,
+    logout,
 } from "./AuthActions";
 import router from "next/router";
+import jwt_decode from "jwt-decode";
+
+const saveLocalStorageAndSetTimeOut = (responseData, dispatch) => {
+    const { user, token } = responseData;
+    const { exp } = jwt_decode(token);
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("accessToken", token);
+    // Convert to milliseconds to compare Date.now()
+    localStorage.setItem("expiredAt", exp * 1000);
+    console.log(exp * 1000 - Date.now());
+    let id = setTimeout(() => {
+        dispatch(logout());
+        localStorage.removeItem("user");
+        localStorage.removeItem("accessToken");
+        localStorage.removeItem("expiredAt");
+        alert("Session expired. Please login again !!!");
+        clearInterval(id);
+        router.push("/");
+    }, exp * 1000 - Date.now());
+};
 
 export const login = async (userCredentials, dispatch) => {
     dispatch(authenStart());
@@ -15,8 +36,7 @@ export const login = async (userCredentials, dispatch) => {
         const res = await axios.post("/users/login", userCredentials);
         console.log(res.data.data);
         dispatch(authenSuccess(res.data.data));
-        localStorage.setItem("user", JSON.stringify(res.data.data.user));
-        localStorage.setItem("accessToken", res.data.data.token);
+        saveLocalStorageAndSetTimeOut(res.data.data, dispatch);
         router.push("/");
     } catch (err) {
         dispatch(authenFailure(err.response.data.message));
@@ -44,8 +64,7 @@ export const signup = async (userCredentials, dispatch) => {
         const res = await axios.post("/users/signup", userCredentials);
         console.log(res.data.data);
         dispatch(authenSuccess(res.data.data));
-        localStorage.setItem("user", JSON.stringify(res.data.data.user));
-        localStorage.setItem("accessToken", res.data.data.token);
+        saveLocalStorageAndSetTimeOut(res.data.data, dispatch);
         router.push("/");
     } catch (err) {
         dispatch(authenFailure(err.response.data.message));
