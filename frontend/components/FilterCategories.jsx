d359889a09a9d326dc67af66f92ba5bdc3aedb2d
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2021-08-26 14:14:27 +0700

    layout update

--- frontend/components/FilterCategories.jsx
+++ frontend/components/FilterCategories.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import Card from "./Card";
-import Carousel from "react-grid-carousel";
+import SearchCard from "./searchResult/SearchCard";
 
 export default function FilterCategories({ rooms }) {
     return (
@@ -10,56 +9,22 @@ export default function FilterCategories({ rooms }) {
                     <h1 className="font-bold text-4xl pb-4">
                         {rooms[0].category.name} Courses
                     </h1>
-                    <div className="mt-4">
+                    <div className="mt-6">
                         <h1 className="font-bold text-2xl m-4 mt-0">
                             Courses to get you started
                         </h1>
-                        <Carousel
-                            cols={5}
-                            responsiveLayout={[
-                                {
-                                    breakpoint: 1600,
-                                    cols: 5,
-                                    rows: 1,
-                                    gap: 10,
-                                    loop: true,
-                                },
-                                {
-                                    breakpoint: 1280,
-                                    cols: 4,
-                                    rows: 1,
-                                    gap: 10,
-                                    loop: true,
-                                },
-                                {
-                                    breakpoint: 1100,
-                                    cols: 3,
-                                    rows: 1,
-                                    gap: 10,
-                                    loop: true,
-                                },
-                                {
-                                    breakpoint: 768,
-                                    cols: 2,
-                                    rows: 1,
-                                    gap: 10,
-                                    loop: true,
-                                },
-                            ]}
-                        >
-                            {rooms.map((room) => (
-                                <Carousel.Item key={room._id}>
-                                    <Card key={room._id} props={room} />
-                                </Carousel.Item>
-                            ))}
-                        </Carousel>
+                    </div>
+                    <div className="mt-6 grid gap-x-4 gap-y-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                        {rooms.map((room) => (
+                            <SearchCard key={room._id} room={room} />
+                        ))}
                     </div>
                 </div>
             )}
             {rooms.length == 0 && (
                 <div className="container mx-auto p-5 lg:p-14">
                     <h1 className="font-bold text-4xl pb-4">
-                        No course available.
+                        No course available for {rooms[0].category.name}
                     </h1>
                 </div>
             )}
