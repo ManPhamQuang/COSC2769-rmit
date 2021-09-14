31f53f87ee2af294b30f7df7ead052cc5938e33c
Vu Thang <dangvuthang97@gmail.com>
2021-09-14 15:05:49 +0700

    Add package to improve backend security

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const compression = require("compression");
 const roomRoute = require("./route/roomRoute");
 const userRoute = require("./route/userRoute");
 const categoryRoute = require("./route/categoryRoute");
@@ -15,6 +21,26 @@ const app = express();
 app.use(cors());
 app.post("/webhook", express.raw({ type: "application/json" }), handleWebHook);
 app.use(express.json());
+
+// Prevent DDOS
+app.use(
+  "/api",
+  rateLimit({
+    max: 500,
+    windowMs: 1000 * 60 * 60,
+    message: "Too many requests. Please try again after an hour",
+  })
+);
+
+app.use(helmet());
+// Prevent query selector injection attacks
+app.use(mongoSanitize());
+// Prevent XSS attacks
+app.use(xss());
+// Prevent HTTP Parameter Pollution attacks
+app.use(hpp());
+// Compress response before sending co client
+app.use(compression());
 
 app.use("/api/v1/rooms", roomRoute);
 app.use("/api/v1/users", userRoute);
